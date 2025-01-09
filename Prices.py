2fd574409417c0b8360018217b7adff9e502ad43
João Vitor Firmino <109932115+joaovitorfirmino2812@users.noreply.github.com>
2025-01-09 03:41:44 -0300

    Update prices
    peguei os dados e separei os valores dos timestamps e formatei os timestamps para data hora e minutos para poder utilizar as duas listas para criar o gráfico.

--- Prices.py
+++ Prices.py
@@ -1,24 +1,34 @@
 import requests as rq
 import pandas as pd 
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
-url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana,dogecoin&vs_currencies=usd,brl"
-dados_json = rq.get(url, timeout=3)
+date =[]
+datainicial = datetime(2025, 1, 8).date()
+datafinal = datetime.now().date() #dia atual
+values = []
+timestamps = []
+tempoMinuto = []
 
-dados = dados_json.json()
+while datafinal >= datainicial:
+    date.append(datafinal)
+    datafinal -= timedelta(days=1)
 
-#BITCOIN
-btcDolar = dados['bitcoin']['usd']
-btcReal = dados['bitcoin']['brl']
+for e in date:
+    url = f"https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1&date={e}"
+    dados_json = rq.get(url, timeout=20)
+    dados = dados_json.json()
+    valores = dados['prices']
+    values.append([valor[1] for valor in valores ])
+    timestamps.append([tamp[0] for tamp in valores])
 
-#ETHEREUM
-ethDolar = dados['ethereum']['usd']
-ethReal = dados['ethereum']['brl']
+for ts in timestamps[0]:
+    #Converter para datetime
+    dt = datetime.fromtimestamp(ts / 1000)  # Dividindo por 1000 para converter ms para s
+    minutos = dt.strftime("%Y-%m-%d %H:%M")  # Formatar como HH:MM (hora:minuto)
+    tempoMinuto.append(minutos)
 
-#SOLANA
-solDolar = dados['solana']['usd']
-solReal = dados['solana']['brl']
-
-#DODGECOIN
-dogeDolar = dados['dogecoin']['usd']
-dogeReal = dados['dogecoin']['brl']
+plt.plot(tempoMinuto, values[0])
+plt.xticks([tempoMinuto[0], tempoMinuto[-1]])
+plt.show()
+print(date)
